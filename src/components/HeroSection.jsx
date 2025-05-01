2e738b251a10b3dd67d0a8dc035e5f18c3f60376
Quake <142627174+SJ-209@users.noreply.github.com>
2025-05-01 15:01:55 +0100

    Microsoft edge addon button added in

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,16 +11,24 @@ function HeroSection() {
         </h1>
         <p className="middleText text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
         Quickly create, organize and access chat folders for Google Gemini.<br/>
-        Fast Folders: The Ultimate Google Gemini Chat Organizer!
+        Fast Folders: The Ultimate Google Gemini Chat Organizer! Available now in the Google Chrome store and Microsoft Edge addons.
         </p>
-        <div className="flex flex-col sm:flex-row gap-4 justify-center mb-12">
+        <div className="flex flex-col sm:flex-row gap-12 justify-center mb-12">
           <a
             href="https://chromewebstore.google.com/detail/fast-folders-the-ultimate/dgmakhnmibfdnkhopleclbfmfncdmfhf"
             className="px-8 py-4 bg-[linear-gradient(45deg,_#4796e3_20%,_#9177c7_50%,_#ca6673_85%)] text-white rounded-lg hover:scale-110 transform transition duration-500 flex items-center justify-center gap-2 pulsingButton"
             target="_blank" // Opens in a new tab
             rel="noopener noreferrer" // Security best practice
           >
-            Add to Chrome <ArrowRight className="w-5 h-5" />
+            Add to Chrome
+          </a>
+          <a
+            href="https://microsoftedge.microsoft.com/addons/detail/google-gemini-folders-c/cgligfihhfhhcjbjadfnlgjcejgjjmlp"
+            className="px-8 py-4 bg-[linear-gradient(45deg,_#4796e3_20%,_#9177c7_50%,_#ca6673_85%)] text-white rounded-lg hover:scale-110 transform transition duration-500 flex items-center justify-center gap-2 pulsingButton"
+            target="_blank" // Opens in a new tab
+            rel="noopener noreferrer" // Security best practice
+          >
+            Add to Edge
           </a>
         </div>
         <div id="video" className="flex justify-center">
