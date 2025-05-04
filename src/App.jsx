c8f830908ee434271f5d64c1d68e8dbe4df9a433
Quake <142627174+SJ-209@users.noreply.github.com>
2025-05-04 09:15:50 +0100

    Uninstall feedback from and thank you form added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import FeaturesSection from './components/FeaturesSection';
 import PricingSection from './components/PricingSection';
-import SecuritySection from './components/SecuritySection';
+// import SecuritySection from './components/SecuritySection';
 import ScrollingReviews from './components/scrolling-reviews';
 import Footer from './components/Footer';
 
@@ -17,6 +17,7 @@ function App() {
       <FeaturesSection />
       <PricingSection />
       <ScrollingReviews />
+      {/* <UninstallFeedback /> */}
       {/* <SecuritySection /> */}
       <Footer />
     </div>
