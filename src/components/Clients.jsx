8f9a955fcaa3514407f51c25af0964987a483c6a
Quake <142627174+SJ-209@users.noreply.github.com>
2025-05-21 19:42:59 +0100

    styles change

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -16,14 +16,14 @@ const clients = [
 
 const Clients = () => {
   return (
-    <section id="clientWork" className="py-20 px-6 md:px-10 bg-white">
+    <section id="clientWork" className="py-20 px-6 md:px-10">
        <div className="clients w-full mb-8 flex justify-center"> 
               <span className="font-bold text-4xl">These Companies Use Fast Folders</span>
             </div>
-        <div className="max-w-6xl mx-auto px-6">
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-10 gap-y-8">
+        <div className="max-w-6xl mx-auto px-6 ">
+            <div className="grid grid-cols-2 justify-center sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-10 gap-y-8">
             {clients.map((client, index) => (
-                <div key={client.name} className="flex items-center justify-center">
+                <div key={client.name} className="flex items-center">
                 <div className=" px-4 py-2 w-full text-center">
                     <img
                     src={client.logo}
