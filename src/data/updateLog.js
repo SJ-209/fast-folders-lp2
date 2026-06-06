b9d0746101b9198d51beefdbdfbd6795070553a4
Quake <142627174+SJ-209@users.noreply.github.com>
2026-06-06 09:12:42 +0100

    updates

---
+++ src/data/updateLog.js
@@ -0,0 +1,10 @@
+const updateLog = [
+    {
+        date: "19 Apr 2026",
+        title: "Fast Folders 1.1.1",
+        features: [
+            "Add Scroll bar to folder list",
+            "Add Email button to footer",
+        ],
+    },
+];
